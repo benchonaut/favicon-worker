5688e7369bb5219dc47ab19dec35b402b299c685
Benjamin Freitag <12718580+benchonaut@users.noreply.github.com>
2024-02-19 23:49:20 +0100

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,22 +1,39 @@
-async function callApi(url: string): Promise<Response> {
-  try {
-    return await fetch(url);
-  } catch (e) {
-    throw new Error(e.message);
-  }
-}
+import { Hono } from "https://deno.land/x/hono@v3.4.1/mod.ts";
 
-callApi("https://api.github.com/users/denoland")
-  .then((response) => {
-    return response.json();
-  })
-  .then(json => console.log(json));
+const app = new Hono();
+const kv = await Deno.openKv();
 
-callApi("https://deno.land/")
-  .then((response) => {
-    return response.text();
-  })
-  .then(text => console.log(text));
+// Redirect root URL
+app.get("/", (c) => c.redirect("/books"));
 
-callApi("https://does.not.exist/")
-  .catch(error => console.log(error));
+// List all books
+app.get("/books", async (c) => {
+  const iter = await kv.list({ prefix: ["books"] });
+  const books = [];
+  for await (const res of iter) books.push(res);
+
+  return c.json(books);
+});
+
+// Create a book (POST body is JSON)
+app.post("/books", async (c) => {
+  const body = await c.req.json();
+  const result = await kv.set(["books", body.title], body);
+  return c.json(result);
+});
+
+// Get a book by title
+app.get("/books/:title", async (c) => {
+  const title = c.req.param("title");
+  const result = await kv.get(["books", title]);
+  return c.json(result);
+});
+
+// Delete a book by title
+app.delete("/books/:title", async (c) => {
+  const title = c.req.param("title");
+  await kv.delete(["books", title]);
+  return c.text("");
+});
+
+Deno.serve(app.fetch);
